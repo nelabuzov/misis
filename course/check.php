3dbc9177b00b0d6e4479a4e9292794334b123c7c
thelabuzov <pozetiv4ik171@yandex.ru>
2023-02-06 20:46:18 +0300

    add account reg script

--- course/check.php
+++
@@ -1,3 +0,0 @@
-<?php
-  $email = filter_var(trim($_POST['email']), FILTER_SANITIZE_STRING);
-?>
---
+++ course/php/check.php
@@ -0,0 +1,22 @@
+<?php
+  $email = filter_var(trim($_POST['email']), FILTER_SANITIZE_STRING);
+
+  $password = filter_var(trim($_POST['password']), FILTER_SANITIZE_STRING);
+
+  if(mb_strlen($email) < 5 || mb_strlen($email) > 50) {
+    echo "Недопустимая длина почты";
+    exit();
+  } else if(mb_strlen($password) < 5 || mb_strlen($password) > 50) {
+    echo "Недопустимая длина пароля (от 5 до 50 символов)";
+    exit();
+  }
+
+  $password = md5($password."fa32tro8");
+
+  $mysql = new mysqli('localhost', 'root', '', 'workflow');
+  $mysql -> query("INSERT INTO `account` (`email`, `password`) VALUES('$email', '$password')");
+
+  $mysql -> close();
+
+  header('Location: /');
+?>
