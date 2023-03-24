0ea8ac869ae0f2126a441bec53e48c04708704e2
Dmitry Labuzov <pozetiv4ik171@yandex.ru>
2023-03-24 23:45:39 +0530

    Add files via upload

---
+++ practice/5/PR5/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PR5
+{
+    //Интерфейс с методом Voice
+    interface IAnimal
+    {
+        void Voice();
+    }
+
+    //Классы с интерфейсом IAnimal
+    class Dog : IAnimal
+    {
+        public void Voice()
+        {
+            Console.WriteLine("Гав!");
+        }
+    }
+
+    class Cat : IAnimal
+    {
+        public void Voice()
+        {
+            Console.WriteLine("Мяу!");
+        }
+    }
+
+    class Pigeon : IAnimal
+    {
+        public void Voice()
+        {
+            Console.WriteLine("Курлык!");
+        }
+    }
+    class Sparrow : IAnimal
+    {
+        public void Voice()
+        {
+            Console.WriteLine("Вик!");
+        }
+    }
+
+    class Owl :  IAnimal
+    {
+        //Чтение времени(Часа) из файла
+        private int GetCurrentTime()
+        {
+            return Convert.ToInt32(File.ReadAllText("current_time.txt"));
+        }
+
+        //Проверка времени и воспроизведение звука совы
+        public void Voice()
+        {
+            int currentTime = GetCurrentTime();
+
+            if((currentTime >=8) && (currentTime <=21))
+            {
+                Console.WriteLine("Тише, я сплю!");
+            }
+            else
+            {
+                Console.WriteLine("Ух! Ух! Ух!");
+            }
+        }
+    }
+
+    class Program
+    {
+        //Статический метод
+        static void PetAnimal(IAnimal animal)
+        {
+            Console.WriteLine("Мы гладим зверушку, а она нам говорит: ");
+             animal.Voice();
+        }
+
+        static void Main(string[] args)
+        {
+            //Приветствие пользователя
+            Console.WriteLine("Вас приветсвует наш гладильный зоопарк!");
+
+            //Инициализация объектов с интерфейсом и дочерним классом
+            Dog tuzik = new Dog();
+            PetAnimal(tuzik);
+
+            Cat boris = new Cat();
+            PetAnimal(boris);
+
+            IAnimal hewdig = new Owl();
+            PetAnimal(hewdig);
+
+            IAnimal sparrow = new Sparrow();
+            PetAnimal(sparrow);
+
+            IAnimal pigeon = new Pigeon();
+            PetAnimal(pigeon);
+
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ practice/5/PR52/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR52
+{
+    //Интерфейс фильтра
+    interface IFilter
+    {
+        string Execute(string textLine);
+    }
+
+    //Класс "Числовой фильтр"
+    class DigitFilter : IFilter
+    {
+        public string Execute(string textline)
+        {
+            //Возвращение строки без чисел, с помощью метода Split и его аргумента
+            return string.Concat(textline.Split("1234567890".ToCharArray(), StringSplitOptions.RemoveEmptyEntries));
+        }
+    }
+
+    //Класс "Буквенный фильтр"
+    class LetterFilter : IFilter
+    {
+       public string Execute(string textline)
+        {
+            //Возвращение строки без букв, с помощью метода Split и его аргумента
+            return string.Concat(textline.Split("АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯяAaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz"
+                .ToCharArray(), StringSplitOptions.RemoveEmptyEntries));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Инициализация объектор с интерфейсом
+            IFilter digfilter = new DigitFilter();
+            IFilter letterfilter = new LetterFilter();
+
+            //Инициализация переменной
+            string outString;
+
+            //Приветствие пользователя
+            Console.Write("Вас приветствует \"Kluger Filter\"!" +
+                "\nВозможности:" +
+                "\n* Удаление букв кириллицы/латиницы;" +
+                "\n* Удаление чисел" +
+                "\n" +
+                "\nВыберите действие (Б/Ч/В): ");
+
+            //Вввод выбора
+            char answ = char.Parse(Console.ReadLine());
+
+            //Ввод полезного тела
+            Console.Write("Введите вашу строку: ");
+            string inString = Console.ReadLine();
+
+            //Выбор и удаление символов
+            if (answ == 'Б' | answ == 'б')
+                outString = letterfilter.Execute(inString);
+            else if (answ == 'Ч' | answ == 'ч')
+                outString = digfilter.Execute(inString);
+            else if (answ == 'В' | answ == 'в')
+                outString = letterfilter.Execute(digfilter.Execute(inString));
+            else
+                outString = "Ошибка, в выборе";
+
+            //Вывод рабочего тела
+            Console.WriteLine(outString);
+            Console.ReadKey();
+        }
+    }
+}
