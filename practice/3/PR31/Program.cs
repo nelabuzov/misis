6f616792f78bac1722b4c36850166710d5aea7b2
Dmitry Labuzov <pozetiv4ik171@yandex.ru>
2023-03-25 12:54:53 +0300

    Update Program.cs

--- practice/3/PR31/Program.cs
+++ practice/3/PR31/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,16 +6,17 @@ using System.Threading.Tasks;
 
 namespace PR31
 {
-    //Класс отвечающий за отправку сообщений
+
+    // Класс для отправки сообщений
     class SmsMessage
     {
-        
-     //Инициализация свойств класса
+
+     // Инициализация свойств класса
      private string messagetxt; 
      public string MessageText { get {  return messagetxt; }  set { messagetxt = CheckLongMess(value); } }
      public double Price { get { return MsgPrice(messagetxt); }}
 
-        //Метод проверки длинны сообщения
+        // Метод проверки длины сообщения
         private string CheckLongMess(string msg1)
         {
             if (msg1.Length > 250)
@@ -25,7 +26,7 @@ namespace PR31
             return msg1;
         }
 
-        //Метод расчёта цены сообщения
+        // Метод расчета цены сообщения
         public double MsgPrice(string msg2)
         {
             if (msg2.Length < 65)
@@ -36,25 +37,26 @@ namespace PR31
         }
     }
 
-    //Клиентский код
+    // Клиентский код
     class Program
     {
         static void Main(string[] args)
         {
-            //Инициализация объекта
+
+            // Инициализация объекта
             SmsMessage smstext = new SmsMessage();
 
-            //Приветствие
+            // Приветствие
             Console.Write("Вас приветсвует телеграф-40000! Мы можем отправить ваше сообщение любому человеку, но только до 250 символов." +
                 "\nТарифы:" +
                 "\nСообщения < 65 символов стоят 1.5;" +
                 "\nСообщения > 65 символов стоят 0.5 рублей." +
                 "\nВведите ваше сообщение (до 250 символов):");
 
-            //Ввод сообщения пользователя
+            // Ввод сообщения пользователя
             smstext.MessageText = Console.ReadLine();
 
-            //Вывод о состоянии отправки
+            // Вывод о состоянии отправки
             Console.Write($"Длина вашего сообщения: {smstext.MessageText.Length}:" +
                 $"\nЦена сообщения: {smstext.Price}");
 
