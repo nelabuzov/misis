0181c16e05ebe1e9ae224aa37580e88187f8ef7b
Dmitry Labuzov <pozetiv4ik171@yandex.ru>
2023-03-25 12:51:07 +0300

    Update Program.cs

--- practice/2/PR21/PR21/Program.cs
+++ practice/2/PR21/PR21/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,11 +23,13 @@ namespace PR21
                 return false;
             }
         }
+
         public int GetNumber()
         {
             return n;
         }
     }
+
     class Program
     {
         static void Main(string[] args)
