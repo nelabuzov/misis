5cc1f3af659de14b9d556b5dc1a42774d4dcabe2
Dmitry Labuzov <pozetiv4ik171@yandex.ru>
2023-03-25 13:03:13 +0300

    Update Program.cs

--- practice/4/PR4/Program.cs
+++ practice/4/PR4/Program.cs
@@ -1,48 +1,52 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PR4
-{   //Класс со свойствами заклинания
+{
+
+    // Класс со свойствами заклинания
     class Spell
     {
-        //Инициализация свойств
+
+        // Инициализация свойств
         public int Mana { get; private set; }
         private string Effect { get; set; }
         public string Name { get; private set; }
 
-        //Конструктор, принимающий значения заклинания
+        // Конструктор принимающий значения заклинания
         public Spell (int mana, string effect, string name)
         {
             Mana = mana;
             Effect = effect;
             Name = name;
         }
-        
-        //Метод использования заклинания, вывод эффект от заклинания
+
+        // Метод использования заклинания
         public string Use()
         {
             return Effect;
         }
     }
 
-    //Класс со свойствами мага
+    // Класс со свойствами мага
     class Magician
     {
-        //Свойства класса Маг
+
+        // Свойства класса Маг
         public string Name { get; private set; }
         public int Mana { get; private set; }
 
-        //Конструтор, принимающий значения мага
+        // Конструтор принимающий значения мага
         public Magician(string name, int mana)
         {
             Name = name;
             Mana = mana;
         }
 
-        //Колдонуть с проверкой маны
+        // Колдонуть с проверкой маны
         public void CastSpell(Spell spell)
         {
             if(Mana >= spell.Mana)
@@ -56,18 +60,20 @@ namespace PR4
             }
         }
     }
+
     class Program
     {
         static void Main(string[] args)
         {
-            //Создание объектов заклинаний
+
+            // Создание объектов заклинаний
             Spell alohomora = new Spell(60, "Замок открывается!","Алохомора");
             Spell vingardiumLeviosa = new Spell(60, "Предмет поднимается в воздух!","Вингардиум-Левиоса");
 
-            //Создание объекта Маг
+            // Создание объекта Маг
             Magician garryPotter = new Magician("Гарри Поттер", 100);
 
-            //Использование заклятий
+            // Использование заклятий
             garryPotter.CastSpell(alohomora);
             garryPotter.CastSpell(vingardiumLeviosa);
             
